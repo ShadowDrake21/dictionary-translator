4e3b8ab693f78078dc6793b2375769fcfe804415
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-03-09 23:16:43 +0200

    getWords(), part 2

--- src/app/pages/translate/translate.component.ts
+++ src/app/pages/translate/translate.component.ts
@@ -2,39 +2,82 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
 
 import { DictionaryService } from '../../core/services/dictionary.service';
-import { BehaviorSubject, Subject, map, tap } from 'rxjs';
-import { IDictionaryWord } from '../../shared/models/dictionaty.model';
+import {
+  BehaviorSubject,
+  Subject,
+  debounceTime,
+  distinctUntilChanged,
+  map,
+  switchMap,
+  tap,
+} from 'rxjs';
+import {
+  Definition,
+  IDictionaryWord,
+  License,
+  Meaning,
+  Phonetic,
+} from '../../shared/models/dictionaty.model';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-translate',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './translate.component.html',
   styleUrl: './translate.component.scss',
 })
 export class TranslateComponent implements OnInit {
-  private DictionaryService = inject(DictionaryService);
+  private dictionaryService = inject(DictionaryService);
 
-  words = new BehaviorSubject<IDictionaryWord[]>([]);
+  translateForm = new FormGroup({
+    word: new FormControl('', Validators.required),
+  });
+
+  words$ = new BehaviorSubject<IDictionaryWord[]>([]);
 
   ngOnInit(): void {
-    this.fetchWordData();
+    // this.fetchWordData();
   }
 
   fetchWordData() {
-    // this.DictionaryService.getWords('girlfriend').pipe(
+    // this.dictionaryService.getWords('girlfriend').pipe(
     //   tap(word => console.log(word)),
     //   map((dicrionary: IDictionaryWord[]) =>
     //     dicrionary.map((word: IDictionaryWord) => {
-    //       this.words.next([...this.words.getValue(), word]);
-    //       console.log(this.words);
+    //       this.words$.next([...this.words$.getValue(), word]);
+    //       console.log(this.words$);
     //     })
     //   )
     // );
-    this.DictionaryService.getWords('girlfriend').subscribe(
-      (words: IDictionaryWord[]) => {
-        this.words.next([...words]);
-      }
-    );
+    this.dictionaryService
+      .getWords('girlfriend')
+      .subscribe((words: IDictionaryWord[]) => {
+        this.words$.next([...words]);
+      });
+  }
+
+  onInput() {
+    if (!this.translateForm.value.word) return;
+
+    this.translateForm
+      .get('word')!
+      .valueChanges.pipe(
+        debounceTime(500),
+        distinctUntilChanged(),
+        switchMap((searchTerm: string | null) =>
+          this.dictionaryService.getWords(searchTerm || '')
+        )
+      )
+      .subscribe(
+        (words: IDictionaryWord[]) => this.words$.next([...words]),
+        (err) => console.log('Error: ', err),
+        () => console.log('Completed')
+      );
   }
 }
