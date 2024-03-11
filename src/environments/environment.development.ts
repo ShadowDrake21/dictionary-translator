445eb453790fee7aa2527ac12580dc69cedae58e
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-03-11 15:08:52 +0200

    firebase settings

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,12 @@
-export const environment = {};
+export const environment = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyAdwZu_s-g03cZmIyaPZPkVd_CqgIQdchM',
+    authDomain: 'dictionary-rxjs.firebaseapp.com',
+    databaseURL:
+      'https://dictionary-rxjs-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'dictionary-rxjs',
+    storageBucket: 'dictionary-rxjs.appspot.com',
+    messagingSenderId: '614393387706',
+    appId: '1:614393387706:web:6f444dbc0b1b3ade4e8083',
+  },
+};
