fbd287da0372180a049cf526602e4df5edd1aafe
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-03-12 16:05:44 +0200

    sign-in page: full functionality

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
-import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { DictionaryComponent } from './pages/dictionary/dictionary.component';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/sign-up' },
+  { path: '', pathMatch: 'full', redirectTo: '/sign-in' },
   {
-    path: 'sign-up',
-    component: SignUpComponent,
+    path: 'sign-in',
+    component: SignInComponent,
   },
   {
     path: 'dictionary',
