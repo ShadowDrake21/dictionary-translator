4771112c38de1922736b8af0f9cca85587886d61
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-03-13 08:42:30 +0200

    sign-in: confirm case, btns refactoring; footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-    title = 'book-angular-app';
-}
+export class AppComponent {}
