291bc7069c30ad9724530e354a87d2159eaf971e
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-03-14 09:39:12 +0200

    profile-page: functionality, part 1

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,12 +1,93 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnDestroy, OnInit, inject } from '@angular/core';
+
+import { HeaderComponent } from '../../shared/components/header/header.component';
+import { AuthService } from '../../core/authentication/auth.service';
+import { DatabaseManipulationsService } from '../../core/services/database-manipulations.service';
+import { BehaviorSubject, Subject, switchMap, takeUntil, tap } from 'rxjs';
+import { User } from '@firebase/auth';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HeaderComponent],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  styleUrl: './profile.component.scss',
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit, OnDestroy {
+  private authService = inject(AuthService);
+  private databaseManipulationsService = inject(DatabaseManipulationsService);
 
+  userUid: string | undefined = undefined;
+
+  user$$ = new BehaviorSubject<User | null>(null);
+  destroy$$ = new Subject<void>();
+
+  favourites$$ = new BehaviorSubject<string[]>([]);
+  error$$ = new BehaviorSubject<string | null>(null);
+
+  ngOnInit(): void {
+    this.authService.user$.pipe(takeUntil(this.destroy$$)).subscribe((user) => {
+      if (user) {
+        this.userUid = user.uid;
+        this.user$$.next(user);
+        this.getDictionaryWords();
+      } else {
+        this.cleartUserData();
+      }
+    });
+  }
+
+  getDictionaryWords(): void {
+    this.databaseManipulationsService
+      .getDictionaryWords(this.userUid)
+      .subscribe((result: string[] | null) => {
+        if (result) {
+          this.favourites$$.next(result);
+          console.log(this.favourites$$.getValue());
+        }
+      });
+  }
+
+  // LAST ITEM AND UI
+  onClearFav(word: string) {
+    this.databaseManipulationsService
+      .deleteDictionaryWord(this.userUid, word)
+      .pipe(
+        switchMap(() => {
+          return this.databaseManipulationsService.getDictionaryWords(
+            this.userUid
+          );
+        }),
+        tap((updatedFavourites: string[] | null) => {
+          if (updatedFavourites) {
+            const filteredFavourites = updatedFavourites.filter(
+              (fav) => fav !== word
+            );
+            this.favourites$$.next(filteredFavourites);
+          }
+        })
+      )
+      .subscribe(() => {
+        console.log(`word ${word} deleted`);
+      });
+  }
+
+  onClearAllFavs() {
+    this.favourites$$.next([]);
+    this.databaseManipulationsService
+      .deleteFullUserDictionary(this.userUid)
+      .subscribe(() => console.log('all words deleted'));
+    console.log(this.favourites$$.getValue());
+  }
+
+  cleartUserData(): void {
+    this.favourites$$.next([]);
+    this.error$$.next(null);
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$$.next();
+    this.destroy$$.complete();
+  }
 }
