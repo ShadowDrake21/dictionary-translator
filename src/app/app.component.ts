d3f8d1c3995f859538d1297f02373259ad781e45
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-03-17 10:40:28 +0200

    adaptive: profile and page-not-found

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {
+  ActivatedRoute,
+  NavigationEnd,
+  Router,
+  RouterOutlet,
+} from '@angular/router';
 
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { filter, map, mergeMap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FooterComponent],
+  imports: [CommonModule, RouterOutlet, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  private router = inject(Router);
+
+  pageIdentificator: string = '';
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        map(() => this.router.routerState.root),
+        map((root: any) => {
+          let route = root;
+          while (route.firstChild) {
+            route = route.firstChild;
+          }
+          return route;
+        }),
+        filter((route: any) => route.outlet === 'primary'),
+        mergeMap((route: any) => route.url),
+        map((segments: any) => segments.map((segment: any) => segment.path))
+      )
+      .subscribe((paths: string[]) => {
+        this.pageIdentificator = paths.join('/');
+      });
+  }
+}
