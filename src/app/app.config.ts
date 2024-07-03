cf507e3c86f5c9c43e86ee1146cadffa49d8905a
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-07-03 11:41:26 +0300

    secrets security fix, v.2

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,7 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 
 import { routes } from './app.routes';
-import { environment } from '../environments/environment.development';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
